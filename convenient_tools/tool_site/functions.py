fb18031192ad35f6f6410086ada7c28ea9205eba
mugiyuta <kamui.creator@gmail.com>
2022-08-07 19:56:14 +0900

    commit

--- convenient_tools/tool_site/functions.py
+++ convenient_tools/tool_site/functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import csv,os
+
 from django.http import HttpResponse
 
 def process_file(file_data,word_data):
@@ -12,8 +13,16 @@ def process_file(file_data,word_data):
 
     return df_result
 
-def to_csv(df):
-    response = HttpResponse(content_type='text/csv; charset=UTF-8')
+def to_csv_cp932(df):
+    response = HttpResponse(content_type='text/csv; charset=cp932')
+    response['Content-Disposition'] = 'attachment; filename="result.csv"'
+    
+    df.to_csv(path_or_buf = response, encoding = 'cp932', index=False)
+    
+    return response
+
+def to_csv_utf_8(df):
+    response = HttpResponse(content_type='text/csv; charset=utf-8-sig')
     response['Content-Disposition'] = 'attachment; filename="result.csv"'
     
     df.to_csv(path_or_buf = response, encoding = 'utf-8-sig', index=False)
--- convenient_tools/tool_site/views.py
+++ convenient_tools/tool_site/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.views.generic import TemplateView, View
 from django.shortcuts import render
+from django import forms
 from tool_site.forms import UploadForm
-from tool_site.functions import process_file, to_csv
+from tool_site.functions import process_file, to_csv_cp932, to_csv_utf_8
 import csv, io
 import pandas as pd
 
@@ -20,11 +21,26 @@ def Tool_extractView(request):
             form_data = upload.cleaned_data
             word_data = form_data["word"]
             file = form_data["testfile"]
-            
-            file_data = pd.read_csv(io.StringIO(file.read().decode('utf-8')), delimiter=',')
-            df = process_file(file_data,word_data)
+            print("プリント1")
+            print(file)
+            print("プリント1")
+            try:
+                file_data1 = pd.read_csv(io.StringIO(file.read().decode('cp932')), delimiter=',')
+                print("プリント2")
+                print(file_data1)
+                print("プリント2")
+                df = process_file(file_data1,word_data)
+                response = to_csv_cp932(df)
 
-            response = to_csv(df)
+            except UnicodeDecodeError:
+                file_data2 = pd.read_csv(io.StringIO(file.read().decode('utf-8')), delimiter=',')
+                print("プリント3")
+                print(file_data2)
+                print("プリント3")
+                df = process_file(file_data2,word_data)
+                response = to_csv_utf_8(df)
+                #raise forms.ValidationError('拡張子がcsvのファイルをアップロードしてください')
+
 
             return response
     else:
