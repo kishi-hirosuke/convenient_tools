5aebdcb58ca1dea429212c3da37f07535d75b84d
mugiyuta <kamui.creator@gmail.com>
2022-08-17 21:10:29 +0900

    commit

--- convenient_tools/tool_site/functions.py
+++ convenient_tools/tool_site/functions.py
@@ -1,10 +1,14 @@
+from distutils.log import error
 from tkinter import N
+from urllib import response
 import pandas as pd
 import numpy as np
 import csv, os, io
 from django.http import HttpResponse
+import chardet
 
 '''エンコード識別関数（不使用）
+
 def getEncode(file):
     encs = "cp932 shift_jis utf-8 utf-8-sig euc-jp iso-2022-jp".split()
     for enc in encs:
@@ -26,7 +30,28 @@ def getEncode(filepath):
         return enc
 '''
 
-#csv書き出し関数
+def csv_flow(file, code, columuns, flow):
+    #エンコード識別、読み取り処理
+    read_file = file.read()
+    file_result = chardet.detect(read_file)
+    enc = file_result['encoding']
+    if enc == None:
+        enc = 'cp932'
+    file_data = pd.read_csv(io.StringIO(read_file.decode(enc)), delimiter=',')
+    #処理選択
+    df = flow(file_data, code, columuns)
+    #CSVカプセル化処理
+    type_data = 'text/csv; charset=' + enc
+    response = HttpResponse(content_type=type_data)
+    response['Content-Disposition'] = 'attachment; filename="result.csv"'
+    df.to_csv(path_or_buf = response, encoding = enc, index=False)
+    return response
+
+def flow_1(file_data, code, columuns):
+    df_result = file_data.loc[file_data[columuns].str.contains(code)]
+    return df_result
+
+'''#csv書き出し関数
 def to_csv(df,enc):
     type_data = 'text/csv; charset=' + enc
     response = HttpResponse(content_type=type_data)
@@ -39,5 +64,8 @@ def to_csv(df,enc):
 
 #csv行抽出関数
 def tool_extract_process(file,columuns,code):
-    df_result = file.loc[file[columuns].str.contains(code)]
-    return df_result
+    try:
+        df_result = file.loc[file[columuns].str.contains(code)]
+        return df_result
+    except KeyError:
+        raise print('存在しない列名が入力されています。')'''
--- convenient_tools/tool_site/views.py
+++ convenient_tools/tool_site/views.py
@@ -1,10 +1,15 @@
 import code
+from ctypes import cdll
+from distutils.log import error
+from tabnanny import check
+from urllib import response
 from django.http import HttpResponse
 from django.views.generic import TemplateView, View
 from django.shortcuts import render
 from django import forms
 from tool_site.forms import UploadForm
-from tool_site.functions import tool_extract_process, to_csv
+#from tool_site.functions import tool_extract_process, to_csv
+from tool_site.functions import csv_flow, flow_1
 import csv, io
 import pandas as pd
 import chardet
@@ -23,11 +28,22 @@ def Tool_extractView(request):
 
         if upload.is_valid():
             form_data = upload.cleaned_data
-            file = form_data["testfile"]
+            file, code, columuns = form_data["testfile"], form_data["code"], form_data["columuns"]
+            '''file = form_data["testfile"]
             code = form_data["code"]
-            columuns = form_data["columuns"]
+            columuns = form_data["columuns"]'''
 
-            #文字コード識別
+            try:
+                response = csv_flow(file, code, columuns, flow = flow_1)
+                return response
+            except:
+                context = {
+                    'error_message':'エラーが発生しました。',
+                    'form':upload
+                }
+                return render(request, "tool_extract.html", context)
+
+            '''#文字コード識別
             read_file = file.read()
             result = chardet.detect(read_file)
             enc = result['encoding']
@@ -37,9 +53,18 @@ def Tool_extractView(request):
 
             #読み込み、編集
             file_data = pd.read_csv(io.StringIO(read_file.decode(enc)), delimiter=',')
-            df = tool_extract_process(file_data,columuns,code)
+            try:
+                df = tool_extract_process(file_data,columuns,code)
+            except:
+                error_message = 'error'
+                context = {
+                    'form':upload,
+                    'eee': error_message
+                }
+                return render(request, "tool_extract.html", context)
             response = to_csv(df,enc)
-            return response
+
+            return response'''
 
         else:
             return render(request, "tool_extract.html", {'form':upload})
