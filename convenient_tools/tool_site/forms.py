ef7331963a09f7402030ba87d293ab6ea8f07c92
kishi-hirosuke <kmnrk3131@gmail.com>
2022-08-25 00:43:55 +0900

    csv処理速度高速化、最大容量500MBに変更

--- convenient_tools/tool_site/forms.py
+++ convenient_tools/tool_site/forms.py
@@ -3,7 +3,7 @@ from django.core.validators import FileExtensionValidator
 from django.core.exceptions import ValidationError
 
 def file_size(value):
-    limit = 30*1000*1000
+    limit = 500*1000*1000
     if value.size > limit:
         raise ValidationError(f'ファイルサイズが大きすぎます。{limit/1000/1000}MBより小さいサイズにしてください。')
 
--- convenient_tools/tool_site/functions.py
+++ convenient_tools/tool_site/functions.py
@@ -6,6 +6,7 @@ import numpy as np
 import csv, os, io
 from django.http import HttpResponse
 import chardet
+from charset_normalizer import detect
 import time
 
 def csv_flow(file, code, columuns, flow):
@@ -14,22 +15,42 @@ def csv_flow(file, code, columuns, flow):
     read_file = file.read()
     elapsed_time = time.time() - start
     print (f"ラップ1:{elapsed_time}秒")
-    file_result = chardet.detect(read_file)
+    file_result = detect(read_file)
+    print(file_result)
     enc = file_result['encoding']
-    if enc == None:
+
+    if enc == 'shift-jis' or enc == 'SHIFT-JIS':
+        pass
+    elif enc == 'shift_jis' or enc == 'SHIFT_JIS':
+        pass
+    elif enc == 'utf-8' or enc == 'UTF-8':
+        pass
+    elif enc == 'utf_8' or enc == 'UTF_8':
+        pass
+    elif enc == 'utf-8-sig' or enc == 'UTF-8-SIG':
+        pass
+    elif enc == 'utf_8_sig' or enc == 'UTF_8_SIG':
+        pass
+    else:
         enc = 'cp932'
-    file_data = pd.read_csv(io.StringIO(read_file.decode(enc)), delimiter=',', dtype = 'object')
+
+    print('エンコード:'+enc)
     elapsed_time = time.time() - start
     print (f"ラップ2:{elapsed_time}秒")
+    file_data = pd.read_csv(io.StringIO(read_file.decode(enc)), delimiter=',', dtype = 'object')
+    elapsed_time = time.time() - start
+    print (f"ラップ3:{elapsed_time}秒")
     #処理選択
     df = flow(file_data, code, columuns)
+    elapsed_time = time.time() - start
+    print (f"ラップ4:{elapsed_time}秒")
     #CSVカプセル化処理
     type_data = 'text/csv; charset=' + enc
     response = HttpResponse(content_type=type_data)
     response['Content-Disposition'] = 'attachment; filename="result.csv"'
     df.to_csv(path_or_buf = response, encoding = enc, index=False)
     elapsed_time = time.time() - start
-    print (f"ラップ3:{elapsed_time}秒")
+    print (f"ラップ5:{elapsed_time}秒")
     return response
 
 def flow_1(file_data, code, columuns):
