21ad8ac79b376e3117b0f933954267293e5e263d
kishi-hirosuke <kmnrk3131@gmail.com>
2022-08-28 22:57:58 +0900

    loading click test

--- convenient_tools/tool_site/static/js/script.js
+++ convenient_tools/tool_site/static/js/script.js
@@ -1,1 +1,15 @@
-window.onload
+console.log('test');
+document.getElementById("btn").addEventListener("click", function() {
+    console.log('ボタンをクリック');
+
+    const loader = document.querySelector("#loader");
+    setTimeout(() => {
+        loader.style.visibility = "visible";
+    }, 1000);
+    console.log('ロード中');
+    setTimeout(() => {
+        loader.style.visibility = "hidden";
+    }, 5000);
+    console.log('ロード終了');
+});
+
