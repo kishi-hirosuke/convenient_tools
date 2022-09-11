cfb5c5776845676608d849d970f19c347e0d993e
kishi-hirosuke <kmnrk3131@gmail.com>
2022-09-11 17:42:28 +0900

    csv分割バリデーション実装

--- convenient_tools/convenient_tools/settings.py
+++ convenient_tools/convenient_tools/settings.py
@@ -1,6 +1,3 @@
-import sys
-sys.dont_write_bytecode = True
-
 """
 Django settings for convenient_tools project.
 
--- convenient_tools/tool_site/forms.py
+++ convenient_tools/tool_site/forms.py
@@ -5,7 +5,7 @@ from django.core.validators import FileExtensionValidator
 from django.core.exceptions import ValidationError
 import re
 
-# お問い合わせ
+#お問い合わせ
 class InquiryForm(forms.Form):
     name = forms.CharField(
         label='氏名',
@@ -20,18 +20,18 @@ class InquiryForm(forms.Form):
         max_length=40,
     )
     tel = forms. CharField(
-        label='電話番号',
+        label='電話番号(数字のみ)',
         min_length=9,
         max_length=15,
         widget=forms.TextInput(attrs={
-        'placeholder':'数字のみ入力可能です。',
+        # 'placeholder':'半角数字入力',
         'pattern':'^[0-9]+$'}))
     mail = forms.EmailField(
         label='メールアドレス',
         required=True,
         max_length=50,
         widget=forms.TextInput(attrs={
-        'placeholder':'sample@example.com',
+        # 'placeholder':'sample@example.com',
         'pattern':'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'}))
     kinds = forms.ChoiceField(
         label='お問い合わせ内容',
@@ -50,22 +50,23 @@ class InquiryForm(forms.Form):
         max_length=255,
         widget=forms.Textarea,)
 
-# 処理
+#最大容量
 def file_size(value):
     limit = 400*1000*1000
     if value.size > limit:
         raise ValidationError(f'ファイルサイズが大きすぎます。{limit/1000/1000}MBより小さいサイズにしてください。')
 
+#csv抽出
 class UploadExtract(forms.Form):
     file = forms.FileField(
         validators=[
             FileExtensionValidator(['csv']),
             file_size
-            ]
-    )
+            ])
     columuns = forms.CharField(max_length=255)
     code = forms.CharField(max_length=2000)
 
+#csv分割
 class UploadSplit(forms.Form):
     header_select = forms.ChoiceField(
     label='ヘッダー指定',
@@ -79,6 +80,9 @@ class UploadSplit(forms.Form):
         validators=[
             FileExtensionValidator(['csv']),
             file_size
-            ]
-    )
-    num = forms.CharField(max_length=255)
+            ])
+    num = forms.CharField(
+        max_length=255,
+        widget=forms.TextInput(attrs={
+        # 'placeholder':'半角数字入力',
+        'pattern':'^[0-9]+$'}))
